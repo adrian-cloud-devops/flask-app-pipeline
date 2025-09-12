a6701f8b086d307321f87b58a2892b47dc546671
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2025-09-12 17:48:17 +0200

    Updated port mapping in app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def update_task(task_id):
     return jsonify({"error": "Task not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
