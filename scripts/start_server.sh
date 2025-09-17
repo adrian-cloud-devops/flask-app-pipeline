60591230c1d461ef5efde5ce9082948e704554f3
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2025-09-17 15:24:18 +0200

    updated logging to find root cause of the issue

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Starting Flask app..." >> /home/ec2-user/flask-todo-app/deploy.log 2>&1
 cd /home/ec2-user/flask-todo-app || exit 1
-echo "Current dir: $(pwd)" >> deploy.log 2>&1
-nohup python3 app.py >> deploy.log 2>&1 &
-echo "Flask started with PID $!" >> deploy.log 2>&1
+
+echo ">>> Stopping old Flask app if running..."
+pkill -f "python3 app.py" || true
+
+echo ">>> Starting Flask app..."
+nohup python3 app.py > /home/ec2-user/flask-todo-app/app.log 2>&1 &
+
+echo ">>> Flask started with PID $!" >> /home/ec2-user/flask-todo-app/deploy.log
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo "Running validate..."
-curl -v http://localhost:5000/ || echo "App not responding yet"
+echo ">>> Validating Flask app..." >> /home/ec2-user/flask-todo-app/deploy.log
+
+if pgrep -f "python3 app.py" > /dev/null
+then
+  echo "Process is running" >> /home/ec2-user/flask-todo-app/deploy.log
+else
+  echo "Process is not running!" >> /home/ec2-user/flask-todo-app/deploy.log
+  exit 1
+fi
+
+curl -f http://localhost:5000/ >> /home/ec2-user/flask-todo-app/deploy.log 2>&1 || exit 1
