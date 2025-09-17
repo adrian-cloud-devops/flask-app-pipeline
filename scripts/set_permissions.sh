58e0ed5b9843527d3c73aae9c2974fda94865f36
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2025-09-17 15:38:51 +0200

    updated permissions

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Fixing permissions..."
+echo ">>> Fixing permissions..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/flask-todo-app
 chmod +x /home/ec2-user/flask-todo-app/scripts/*.sh
