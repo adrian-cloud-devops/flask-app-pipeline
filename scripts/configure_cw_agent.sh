49613ec21354b983422f9f38cc0edd070cdd7252
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2025-09-18 16:31:44 +0200

    reconfiguring monitoring agent

--- scripts/configure_cw_agent.sh
+++ scripts/configure_cw_agent.sh
@@ -12,7 +12,6 @@ sudo cp /home/ec2-user/flask-todo-app/config/cw-config.json /opt/aws/amazon-clou
 
 # Start agent
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-  -a fetch-config \
-  -m ec2 \
-  -c file:/opt/aws/amazon-cloudwatch-agent/etc/config.json \
-  -s
+-a start \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/config.json
