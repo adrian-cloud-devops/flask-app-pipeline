ef2e34ec9aec27614bc0c8e2498c9caab92498f9
adrianjozefowski-polsl <56541592+adrianjozefowski-polsl@users.noreply.github.com>
2025-09-20 14:52:01 +0200

    fix path issue within install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,4 +5,5 @@ echo ">>> Installing Python and pip..."
 sudo dnf install -y python3 python3-pip
 
 echo ">>> Installing Python dependencies..."
+cd /home/ec2-user/flask-todo-app 
 pip3 install -r requirements.txt
